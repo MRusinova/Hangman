7511415b9f1b907ce88181c8e946504ef155e500
MRusinova <rusinovamihaela@gmail.com>
2022-11-22 19:02:17 +0000

    Create a sample list of words and create a function to randomly generate words

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
 import random
 
+words = ['chicken', 'dog', 'pasta', 'tomatoe', 'radio']
+lives_remaining = 7
+guessed_letters = ''
 
+def pick_a_word():
+    """
+    Function to generate a random word from our list of strings
+    """
+    word_position = random.randint(0, len(words) - 1)
+    return words[word_position]
+
+print(pick_a_word())
