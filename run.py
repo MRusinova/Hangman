a69edcf28de31e0238c639ecf67e563765e798a6
MRusinova <rusinovamihaela@gmail.com>
2022-11-23 08:37:52 +0000

    Create a function to compare user's input to letters in generated word.Display position of letter if guess is right

--- run.py
+++ run.py
@@ -11,6 +11,21 @@ def pick_a_word():
     word_position = random.randint(0, len(words) - 1)
     return words[word_position]
 
+def print_word_with_blanks(word):
+    """
+    Compares the letter the player entered with every letter of the randomly selected word.
+    Displays if any of the guessed letters are in the generated word.
+    """
+    display_word = ''
+
+    for letter in word:
+        if guessed_letters.find(letter) > -1:
+            display_word = display_word = letter
+        else:
+            display_word = display_word = '-'
+    
+    print(display_word)
+
 def get_guess(word):
     """
     Function to tell how the player is doing when they try and guess
